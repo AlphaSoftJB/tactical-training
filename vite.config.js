b9389d9711bbb0f1bf48a6bb992626b284f51b07
BsoftMaewada <maewada@live.com>
2025-07-13 11:44:25 -0400

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/tactical-training-app/', // Add this line
+  base: '/tactical-training/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
